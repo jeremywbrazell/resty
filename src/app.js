3534a866879e9c274c7c79c7c5d4060ce3c969c0
jeremywbrazell <jwbrazell@gmail.com>
2022-01-05 20:45:14 -0800

    pushing lab without history

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useReducer, useEffect, useState } from "react";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Form from "./components/form/form";
@@ -6,32 +6,58 @@ import Results from "./components/results/results";
 import axios from "axios";
 import "./app.scss";
 
+const initialState = {
+  data: null,
+  requestParams:{}
+}
+
+const reducer = (state = initialState, action) => {
+  console.log('line 15', state, action)
+  switch(action.type) {
+    default: return state;
+    case 'UPDATE_REQUESTPARAMS': return {...state, ...action.payload};
+    case 'UPDATE_DATA': return {...state, ...action.payload}
+  }
+}
+
 function App() {
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  // const [data, setData] = useState(null);
+  // const [requestParams, setRequestParams] = useState({});
 
   const callApi = async (requestParams) => {
-    setRequestParams(requestParams);
+    // setRequestParams(requestParams);
+    const action = {
+      type: 'UPDATE_REQUESTPARAMS',
+      payload: {requestParams}
+    }
+    dispatch(action)
   };
   useEffect(async () => {
-    if (Object.keys(requestParams).length > 0) {
+    if (Object.keys(state.requestParams).length > 0) {
       const response = await axios({
-        method: requestParams.method,
-        url: requestParams.url,
+        method: state.requestParams.method,
+        url: state.requestParams.url,
       })
         .then((res) => res)
         .catch((e) => console.log(e));
-      setData(response);
+        
+      const action = {
+        type: 'UPDATE_DATA',
+        payload: { data: response }
+      }
+      dispatch(action)
     }
-  }, [requestParams]);
+  }, [state.requestParams]);
 
   return (
     <>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
+      <div>Request Method: {state.requestParams.method}</div>
+      <div>URL: {state.requestParams.url}</div>
       <Form handleApiCall={callApi} />
-      <Results data={data} />
+      <Results data={state.data} />
       <Footer />
     </>
   );

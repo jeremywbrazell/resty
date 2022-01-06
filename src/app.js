82305f595ab4d7cf8c8ce1cb34159e21021c9852
jeremywbrazell <jwbrazell@gmail.com>
2022-01-05 19:48:59 -0800

    dynamic axios working

--- src/app.js
+++ src/app.js
@@ -1,41 +1,29 @@
-import React, { useState } from 'react';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Form from './components/form/form';
-import Results from './components/results/results';
-import axios from 'axios';
-import './app.scss';
+import React, { useEffect, useState } from "react";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Form from "./components/form/form";
+import Results from "./components/results/results";
+import axios from "axios";
+import "./app.scss";
 
 function App() {
-
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
- 
-  // componentDidMount() {
-  //   axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
-  //   .then(res => {
-  //     const poke = res.data;
-  //     setState({ poke })
-  //   })
-  // }
+
   const callApi = async (requestParams) => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
-    .then(res => {
-      const poke = res.data;
-      setData({ poke })
-    })
-    // await setTimeout(() => {
-    //   const data = {
-    //     count: 2,
-    //     results: [
-    //       { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-    //       { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-    //     ],
-    //   };
-    //   setData(data);
-    // }, 1000)
     setRequestParams(requestParams);
-  }
+  };
+  useEffect(async () => {
+    if (Object.keys(requestParams).length > 0) {
+      const response = await axios({
+        method: requestParams.method,
+        url: requestParams.url,
+      })
+        .then((res) => res)
+        .catch((e) => console.log(e));
+      setData(response);
+    }
+  }, [requestParams]);
 
   return (
     <>
